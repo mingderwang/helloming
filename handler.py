8af17e337aa08eb45c1d704c0fdae8b409db7a7e
Ming-der Wang <ming@log4analytics.com>
2020-09-30 17:00:36 +0800

    v1.0.1, missing possibility feature

--- handler.py
+++ handler.py
@@ -1,8 +1,16 @@
 import json
 import random
+import uuid
+import time
 
 def handle(event, context):
-    result = json.dumps({"可能中獎號碼": random.sample(range(10),3)}, ensure_ascii=False)
+    result = json.dumps(
+        {
+            "id": str(uuid.uuid4()),
+            "選號日期": time.strftime("%m/%d/%Y", time.localtime()) ,
+            "可能中獎號碼": random.sample(range(10),3),
+            "猜中機率": 0
+        }, ensure_ascii=False)
     return {
         "body": result,
         "statusCode": 200,
